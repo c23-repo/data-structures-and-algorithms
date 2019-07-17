fd936788c1fbe8b087463044b520b0998a6fcb0b
Cristian Restrepo <crestrepo21@gmail.com>
2019-07-17 09:45:33 -0700

    done with the lab

--- code401Challenges/src/test/java/graph/GraphTest.java
+++ code401Challenges/src/test/java/graph/GraphTest.java
@@ -2,7 +2,9 @@ package graph;
 
 import org.junit.Before;
 import org.junit.Test;
+import sun.security.provider.PolicyParser;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 
 import static org.junit.Assert.*;
@@ -33,5 +35,69 @@ public class GraphTest {
         this.testGraph.addNode(3);
     }
 
-    
+    @Test
+    public void testSize() {
+        this.testGraph.addNode(4);
+        this.testGraph.addNode(5);
+        this.testGraph.addNode(6);
+        this.testGraph.addNode(7);
+
+        assertEquals(4, this.testGraph.size());
+    }
+
+    @Test
+    public void testNoNeighbors() {
+        this.testGraph.addNode(4);
+        this.testGraph.addNode(5);
+
+        GraphNode<Integer> node1 = this.testGraph.getNodes().get(0);
+        GraphNode<Integer> node2 = this.testGraph.getNodes().get(1);
+        HashMap<GraphNode<Integer>, Integer> empty = new HashMap<>();
+
+        assertTrue(this.testGraph.getNeighbors(node1).equals(empty));
+        assertTrue(this.testGraph.getNeighbors(node2).equals(empty));
+    }
+
+    @Test
+    public void testEdgeAndNeighbors() {
+        this.testGraph.addNode(4);
+        this.testGraph.addNode(5);
+        this.testGraph.addNode(6);
+        this.testGraph.addNode(7);
+
+        GraphNode<Integer> node1 = this.testGraph.getNodes().get(0);
+        GraphNode<Integer> node2 = this.testGraph.getNodes().get(1);
+        GraphNode<Integer> node3 = this.testGraph.getNodes().get(2);
+
+        this.testGraph.addEdge(node2, node1, 20);
+        this.testGraph.addEdge(node1, node3, 35);
+        this.testGraph.addEdge(node2, node3, 10);
+        HashMap<GraphNode<Integer>, Integer> neighbors = new HashMap<>();
+
+        neighbors.put(node2, 20);
+        neighbors.put(node3, 35);
+        assertTrue(this.testGraph.getNeighbors(node1).equals(neighbors));
+    }
+
+    @Test
+    public void testGetNodes() {
+        this.testGraph.addNode(4);
+        this.testGraph.addNode(5);
+        this.testGraph.addNode(6);
+        this.testGraph.addNode(7);
+
+        ArrayList<GraphNode<Integer>> vertecies = new ArrayList<>();
+        GraphNode<Integer> node1 = this.testGraph.getNodes().get(0);
+        GraphNode<Integer> node2 = this.testGraph.getNodes().get(1);
+        GraphNode<Integer> node3 = this.testGraph.getNodes().get(2);
+        GraphNode<Integer> node4 = this.testGraph.getNodes().get(3);
+
+        vertecies.add(node1);
+        vertecies.add(node2);
+        vertecies.add(node3);
+        vertecies.add(node4);
+
+        assertTrue(this.testGraph.getNodes().equals(vertecies));
+    }
+
 }
